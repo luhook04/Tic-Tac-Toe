a8c723bd02c113c607a1fc44c472db16fdb1daf7
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-10-11 14:13:21 -0400

    started setting up the gameplay and gameBoard objects

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 // player factory function
-const playerFactory = (mark) => mark;
+const playerFactory = (userSign) => {
+  return userSign;
+};
 
 // gameboard object
 const gameBoard = {
-	board = ['', '', '', '', '', '', '', '', '']
-}
+  board      : [ '', '', '', '', '', '', '', '', '' ],
+  render     : function() {},
+  getDomEls  : function() {
+    const squares = document.querySelectorAll('td');
+  },
+  bindEvents : function() {}
+};
+
+// gameplay object
+const gameplay = (() => {
+  const player1 = playerFactory('X');
+  const player2 = playerFactory('O');
+
+  const possibleWinArrays = [
+    [ 0, 1, 2 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ],
+    [ 0, 3, 6 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 0, 4, 8 ],
+    [ 2, 4, 6 ]
+  ];
+})();
