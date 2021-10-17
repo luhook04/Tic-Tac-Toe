56bbe8ba2bafa0851c181d7e794d156a7b2b4d67
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-10-17 17:27:55 -0400

    added some css and also added the render and updateboard functions

--- app.js
+++ app.js
@@ -4,29 +4,39 @@ const playerFactory = (userSign) => {
 };
 
 // gameboard object
-const gameBoard = {
-  board       : [ '', '', '', '', '', '', '', '', '' ],
-  init        : function() {
-    this.getDomEls();
-    this.bindEvents();
-    this.render();
-  },
-  render      : function() {},
-  getDomEls   : function() {
-    this.squares = document.querySelectorAll('td');
-  },
-  bindEvents  : function() {
-    this.squares.forEach((square) => {
-      square.addEventListener('click', this.render.bind(this));
+const gameBoard = (() => {
+  let board = [ '', '', '', '', '', '', '', '', '' ];
+
+  // cache dom
+  const squares = document.querySelectorAll('td');
+
+  // bind events
+  squares.forEach((square) => {
+    square.addEventListener('click', updateBoard);
+  });
+
+  render();
+  // add functions
+  function render() {
+    squares.forEach((square) => {
+      square.textContent = board[square.id];
     });
-  },
-  updateBoard : function() {
-    board[e.target.id] = game.activePlayer;
   }
-  // Add a new method below
-  // This method should be added to every square element
-  // Takes board[e.target.id] and equals it to the active player mark
-};
+
+  function updateBoard(e) {
+    console.log(this);
+    if (board[e.target.id] === '') {
+      board[e.target.id] = gameplay.activePlayer;
+    }
+    else {
+      return;
+    }
+    render();
+    gameplay.changePlayerTurn();
+  }
+
+  return { board, squares };
+})();
 
 // gameplay object
 const gameplay = (() => {
@@ -34,6 +44,8 @@ const gameplay = (() => {
   const player2 = playerFactory('O');
 
   let activePlayer = player1;
+  let isWinner = false;
+  let remainingMoves = 9;
 
   const possibleWinArrays = [
     [ 0, 1, 2 ],
@@ -45,4 +57,17 @@ const gameplay = (() => {
     [ 0, 4, 8 ],
     [ 2, 4, 6 ]
   ];
+
+  function changePlayerTurn() {
+    if (this.activePlayer == player1) {
+      this.activePlayer = player2;
+      console.log(this.activePlayer);
+    }
+    else if (this.activePlayer == player2) {
+      this.activePlayer = player1;
+      console.log(this.activePlayer);
+    }
+  }
+
+  return { activePlayer, changePlayerTurn };
 })();
