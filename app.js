2e62de4353c7d243b091f00513a4c292997a75c9
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-10-18 12:45:13 -0400

    finished debugging and adding methods

--- app.js
+++ app.js
@@ -8,34 +8,38 @@ const gameBoard = (() => {
   let board = [ '', '', '', '', '', '', '', '', '' ];
 
   // cache dom
+  const resetBtn = document.querySelector('#reset');
   const squares = document.querySelectorAll('td');
-
   // bind events
   squares.forEach((square) => {
     square.addEventListener('click', updateBoard);
   });
 
-  render();
-  // add functions
   function render() {
     squares.forEach((square) => {
-      square.textContent = board[square.id];
+      square.textContent = gameBoard.board[square.id];
+      console.log(board);
+      console.log(gameBoard.board);
     });
   }
 
   function updateBoard(e) {
-    console.log(this);
-    if (board[e.target.id] === '') {
-      board[e.target.id] = gameplay.activePlayer;
+    if (
+      gameBoard.board[e.target.id] === '' &&
+      gameplay.isWinner === false
+    ) {
+      gameBoard.board[e.target.id] = gameplay.activePlayer;
     }
     else {
       return;
     }
     render();
+    gameplay.checkWinner();
     gameplay.changePlayerTurn();
+    gameplay.checkTie();
   }
 
-  return { board, squares };
+  return { board, render, resetBtn };
 })();
 
 // gameplay object
@@ -43,6 +47,7 @@ const gameplay = (() => {
   const player1 = playerFactory('X');
   const player2 = playerFactory('O');
 
+  const winDisplay = document.querySelector('#winDisplay');
   let activePlayer = player1;
   let isWinner = false;
   let remainingMoves = 9;
@@ -58,16 +63,57 @@ const gameplay = (() => {
     [ 2, 4, 6 ]
   ];
 
+  function checkWinner() {
+    possibleWinArrays.forEach((array) => {
+      if (
+        gameBoard.board[array[0]] == this.activePlayer &&
+        gameBoard.board[array[1]] == this.activePlayer &&
+        gameBoard.board[array[2]] == this.activePlayer
+      ) {
+        this.isWinner = true;
+        this.winDisplay.textContent = this.activePlayer + ' wins!';
+      }
+      else return;
+    });
+  }
+
+  function checkTie() {
+    if (this.remainingMoves == 0 && this.isWinner == false) {
+      winDisplay.textContent = 'Tie Game!';
+    }
+    else return;
+  }
+
   function changePlayerTurn() {
     if (this.activePlayer == player1) {
       this.activePlayer = player2;
-      console.log(this.activePlayer);
+      this.remainingMoves--;
     }
     else if (this.activePlayer == player2) {
       this.activePlayer = player1;
-      console.log(this.activePlayer);
+      this.remainingMoves--;
     }
   }
 
-  return { activePlayer, changePlayerTurn };
+  function newGame() {
+    gameplay.winDisplay.textContent = '';
+    gameplay.isWinner = false;
+    gameplay.activePlayer = 'X';
+    gameplay.remainingMoves = 9;
+    gameBoard.board = [ '', '', '', '', '', '', '', '', '' ];
+    gameBoard.render();
+  }
+
+  gameBoard.resetBtn.addEventListener('click', newGame);
+
+  return {
+    activePlayer,
+    changePlayerTurn,
+    checkWinner,
+    isWinner,
+    remainingMoves,
+    checkTie,
+    newGame,
+    winDisplay
+  };
 })();
